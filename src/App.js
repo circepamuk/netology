7bf840f0f464a8a37598ac100d65585f286af348
Oleksandra S <aleksandra.ua.vas@gmail.com>
2023-01-17 19:22:11 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
+import "./App.css";
+import { PATHS } from "./constants";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <div className="leftAside">
+          <ul>
+            {PATHS.map((item, key) => (
+              <Link key={`${key}${item.name}`} to={item.path}>
+                <li>{item.name}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className="content__container">
+          <Routes>
+            {PATHS.map((item, key) => (
+              <Route
+                path={item.path}
+                key={`${key}${item.path}`}
+                element={
+                  <HomeworkWrapper name={item.name} stylePath={item.stylePath}>
+                    {item.component()}
+                  </HomeworkWrapper>
+                }
+              />
+            ))}
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
+const HomeworkWrapper = (props) => {
+  return (
+    <div className="homework__wrapper">
+      <div className="homework__title">{props.name}</div>
+      <link rel="stylesheet" type="text/css" href={props.stylePath} />
+      {props.children}
+    </div>
+  );
+};
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,17 @@
+import Homework1 from "./Homeworks/Homework1_1/Index";
+import Homework1_1 from "./Homeworks/Homework1_2/Index";
+
+export const PATHS = [
+  {
+    path: "/homework1_1",
+    name: "Домашнее задание №1.1",
+    component: Homework1,
+    stylePath: "homework1.css",
+  },
+  {
+    path: "/homework1_2",
+    name: "Домашнее задание №1.2",
+    component: Homework1_1,
+    stylePath: "homework1.css",
+  },
+];
